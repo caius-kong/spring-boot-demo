3c92b98d042b47f79d8c14896ab5774b462e1f49
孔昀晖 <1032316751@qq.com>
2017-08-30 10:16:12 +0800

    init

---
+++ src/main/java/com/example/demo/rest/SimpleController.java
@@ -0,0 +1,16 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by kongyunhui on 2017/8/28.
+ */
+@RestController
+@RequestMapping("/")
+public class SimpleController {
+    @RequestMapping("")
+    public String index(){
+        return "index";
+    }
+}
