4bca46b824ab93f4ef685aaa526c3195c1e82880
孔昀晖 <1032316751@qq.com>
2017-08-30 17:54:52 +0800

    ci/cd test 2

--- src/main/java/com/example/demo/rest/SimpleController.java
+++ src/main/java/com/example/demo/rest/SimpleController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleController {
     @RequestMapping("")
     public String index(){
-        return "index2";
+        return "index3";
     }
 }
